3a37f13badc5b2badbb656263ed13bd81159db21
Rohit Bhure <rohitbhure.cse@gmail.com>
2025-08-06 12:25:48 +0530

    signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
-import { TwowaybindingComponent } from './twowaybinding/twowaybinding.component';
+import { Component, signal } from '@angular/core';
+import { SignalComponent } from './signal/signal.component';
 // import { TemplatereferencevariableComponent } from './templatereferencevariable/templatereferencevariable.component';
 // import { GetandsetComponent } from './getandset/getandset.component';
+// import { TwowaybindingComponent } from './twowaybinding/twowaybinding.component';
+// import { DynamicstylingngstyleandngclassComponent } from './dynamicstylingngstyleandngclass/dynamicstylingngstyleandngclass.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     // GetandsetComponent
     // TemplatereferencevariableComponent,
-    TwowaybindingComponent
+    // TwowaybindingComponent
+    // DynamicstylingngstyleandngclassComponent
+    SignalComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'

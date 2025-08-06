adf7483b2e22d6febc2443a93735ac1c90ef02e2
Rohit Bhure <rohitbhure.cse@gmail.com>
2025-08-06 12:10:19 +0530

    commit-

---
+++ src/app/dynamicstylingngstyleandngclass/dynamicstylingngstyleandngclass.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NgClass,NgStyle } from '@angular/common';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-dynamicstylingngstyleandngclass',
+  imports: [NgClass, NgStyle, CommonModule],
+  templateUrl: './dynamicstylingngstyleandngclass.component.html',
+})
+export class DynamicstylingngstyleandngclassComponent {
+ // For ngClass
+  isActive = true;
+  isHighlighted = false;
+  hasError = true;
+  classList = ['box', 'rounded'];
+
+  // For ngStyle
+  textColor = 'red';
+  fontSize = 16;
+  isError = false;
+  dynamicWidth = '200px';
+}
